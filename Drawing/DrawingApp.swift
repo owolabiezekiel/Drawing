85b92663191d8ae1175a3ce731e9339c4e803cf1
Owolabi Tobiloba <owo.ezekiel@gmail.com>
2021-05-08 01:00:02 +0100

    Initial Commit

---
+++ Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Tobiloba Owolabi on 08/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
